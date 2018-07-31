37044003c8f3f8141b106ea91627fed7a6daf2f8
Raagul N <40118538+beyondtheinferno@users.noreply.github.com>
2018-08-01 00:14:08 +0530

    Test files for sensors
    Each file is to check individual sensors separately for proper functioning before integrating it into the project.

---
+++ sensor-test/motor_test/motor_test.ino
@@ -0,0 +1,52 @@
+const int leftForward = 8; 
+const int leftBackward = 9; 
+const int rightForward = 11; 
+const int rightBackward = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(leftForward , OUTPUT);
+  pinMode(leftBackward , OUTPUT);
+  pinMode(rightForward , OUTPUT);
+  pinMode(rightBackward , OUTPUT);
+}
+
+void loop() {
+  forward();
+  delay(2000);
+  backward();
+  delay(2000);
+  left();
+  delay(2000);
+  right();
+  delay(2000);
+}
+
+void forward() {
+  digitalWrite(leftForward , HIGH);
+  digitalWrite(leftBackward , LOW);
+  digitalWrite(rightForward , HIGH);
+  digitalWrite(rightBackward , LOW);  
+}
+
+void backward() {
+  digitalWrite(leftForward , LOW);
+  digitalWrite(leftBackward , HIGH);
+  digitalWrite(rightForward , LOW);
+  digitalWrite(rightBackward , HIGH);  
+}
+
+void left() {
+  digitalWrite(leftForward , LOW);
+  digitalWrite(leftBackward , LOW);
+  digitalWrite(rightForward , HIGH);
+  digitalWrite(rightBackward , LOW);  
+}
+
+void right() {
+  digitalWrite(leftForward , HIGH);
+  digitalWrite(leftBackward , LOW);
+  digitalWrite(rightForward , LOW);
+  digitalWrite(rightBackward , LOW);  
+}
+
---
+++ sensor-test/ultra_test/ultra_test.ino
@@ -0,0 +1,30 @@
+const int echoPin = 7;
+const int trigPin = 6;
+
+long duration;
+int distance;
+int d;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  d = ultra();
+  Serial.print("Distance: ");
+  Serial.println(d);
+}
+
+int ultra(){
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  return distance;
+}
+
